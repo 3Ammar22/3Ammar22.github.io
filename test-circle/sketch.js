3f7b9137ef50499e8aa0b3ab7d5bbb1b1063d918
Ammar <Ammar.rfai4@gmail.com>
2020-10-05 10:00:11 +0200

    Cirkel

---
+++ test-circle/sketch.js
@@ -0,0 +1,47 @@
+
+let x = 0;
+let hastighed = 3
+let radius = 50
+function setup() {
+  createCanvas(600, 400);
+
+}
+
+
+function draw() {
+  background(220);
+  circle (150,x,radius);
+// her sker det at den første if får vores x koordinat
+// til at være -3 når vores x større en bredden
+// den anden if siger nu hvor x mindre end 0
+// så skal vores x være større end vores x værdig
+// - radius/2 gør det at den rammer kenten med uderside
+
+  //vandret HUSK AT BYT X OG 150 RUNDT
+// if (x > width - radius/2){
+//hastighed = -3;
+//}
+//if (x < 0 + radius/2){
+//hastighed = 3;
+//}
+
+//__________________________________________
+  if (x < length + radius/2){
+    hastighed = 5;
+    }
+    if (x > mouseY - radius/2){
+    hastighed = -5;
+    }
+
+ //  if (x > width || x < 0) {
+  //   hastighed = hastighed * -1.1; }
+  
+  x = x + hastighed;   
+
+}
+  
+
+
+   
+   
+
